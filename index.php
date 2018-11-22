3f0abb391a4737697ed1a0f43e09ce03ee9c45f8
Vitalii <9455469@mail.ru>
2018-11-22 12:50:07 +0300

    7.1

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<? 
+
+$url = $_SERVER['REQUEST_URI'];
+$title = 'калькулятор площади пола онлайн';
+	
+if($url == '/calculator/area_apartment')	{ $title = 'калькулятор площади квартиры онлайн 3D'; }
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,7 +14,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Webgl-editor</title>
+  <title><?=$title?></title>
   
   
   <style type="text/css">
@@ -31,7 +40,11 @@ iframe {position:absolute;top:0;left:0;bottom: 0;right: 0;width:100%; height:100
 	<script>console.log('Start Metrika', window.location.hostname)</script>
 	<!-- Yandex.Metrika counter --><script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter11007949 = new Ya.Metrika({id:11007949, webvisor:true, clickmap:true, trackLinks:true, accurateTrackBounce:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/11007949" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
 <?}else{?>
-	<script>console.log('Stop Metrika', window.location.hostname)</script> 
+	<script>
+	console.log('Stop Metrika', window.location.hostname);
+	console.log("<?echo $url?>");
+	console.log("<?echo $title?>");
+	</script> 
 <?}?>
 
 </html>
